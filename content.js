d7b83b9c0f4edf0f1d5f2fac3814ea2cc7b94998
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-11-09 23:41:38 -0800

    No longer using chance

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', function () {
     if (window.location.href.includes("dylanp369")) {
-
-        var allHisStuff = document.body.innerHTML.split(" ");
+        var allHisStuff = [];
+        allHisStuff = document.body.innerHTML.split(" ");
         newString = "";
         for (var i = 0; i < allHisStuff.length; i++) {
-            if (allHisStuff[i].toLocaleLowerCase == "bee") {
+            if (["bee", "bees", "buzz"].includes(allHisStuff[i].toLowerCase)) {
+                console.log("Reach");
                 newString += '<h1 class="crazy">' + chance.word() + ' <h1>'
             } else {
                 newString += allHisStuff[i] + " ";
