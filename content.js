2f52da4a6eae49ca6e4bd4f89f256cd18fc28fbb
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-11-12 21:03:47 -0800

    add html support

--- content.js
+++ content.js
@@ -9,14 +9,14 @@ window.addEventListener('load', function () {
         allHisStuff = document.body.innerHTML.split(" ");
         newString = "";
         for (var i = 0; i < allHisStuff.length; i++) {
-            if (["bee", "bees", "buzz", "bee,", "monster", "scream", "screams", "snake", "screams,", "scream,", "shrek", "donkey", "stupid", "love", "donkey,", "monsters", "monster", "monsters,", "monster,", "dylan", "dylan's"].includes(allHisStuff[i].toLowerCase())) {
+            if (["bee", "bees", "buzz", "bee,", "monster", "scream", "screams", "snake", "screams,", "scream,", "shrek", "donkey", "stupid", "love", "donkey,", "monsters", "monster", "monsters,", "monster,", "dylan", "dylan's", ">Dylan's", ">Dylan"].includes(allHisStuff[i].toLowerCase())) {
                 newString += ('<dick class="crazy" style="color: chartreuse;">') + randomWordList[Math.floor(Math.random() * randomWordList.length)] + ' </dick>'
             } else {
                 newString += allHisStuff[i] + " ";
             }
         }
         document.body.innerHTML = newString;
-        if (window.location.href.includes("snake")){
+        if (window.location.href.includes("snake")) {
             var badBoy = this.document.createElement("H1");
             badBoy.style = "color: chartreuse;";
             badBoy.innerHTML = "Bad Boy, Mr.Dunlea is watching you.";
